7aea4051c3c33eb4fad9f34b95e2f9fff8360765
Mr_Mao <wwu710632@gmail.com>
2020-06-22 23:13:17 +0800

    Mr_Mao 2020-6-22 23:12:56 简化wx代码工具库逻辑

--- 100_开发工具库/代码工具/javascript/wx-javascript-utils.js
+++ 100_开发工具库/代码工具/javascript/wx-javascript-utils.js
@@ -2,6 +2,7 @@
  * @param  {string} name wxAPI对应的名称
  * @param  {object} options wxAPI对应的参数
  * @returns {Promise} 返回微信API成功或失败的结果
+ * @example const result = await wxPromise('api名称', {配置参数})
  */
 export const wxPromise = (name = "", options = {}) => new Promise((resolve, reject) => {
     wx[name]({
@@ -11,29 +12,25 @@ export const wxPromise = (name = "", options = {}) => new Promise((resolve, reje
     })
 })
 
-/** 根据CSS选择器字符串获取canvas与参数
- * @param  {string} select_str CSS选择器字符串
- * @param  {object} fields_options fields属性参数
- * @returns {node:object, width:number, height:number} // 返回canvas实例与canvas的尺寸
- */
-export const getCanvasRect = (select_str = '', fields_options = {}) => new Promise((resolve) => {
-    const query = wx.createSelectorQuery()
-    query.select(select_str)
-        .fields({ ...fields_options, node: true, size: true }, function (canvasRect) {
-            resolve(canvasRect)
-        })
-        .exec()
-})
 
 
-/** 获取ctx画笔, 以及往ctx中添加绘制图片的方法和绘制换行的方法
- * @param  {object} node canvas实例
- * @returns {rpx:number, ctx:object} // 返回canvas实例与canvas的尺寸
+/** 创建绘制海报矩形方法, 自动向画笔添加加载绘制图片方法(ctx.loadDrawImage), 绘制换行字体方法(warpFillText)
+ * @param  {string} select_str 查询CSS选择器字符串
+ * @returns {node, ctx, rpx} 返回canvas实例, 画笔, 适配值
+ * @example const {node, ctx, rpx} = await createDrawPosterRect('#canvas')
  */
-export const createDrawRect = (node) => {
+export const createDrawPosterRect = async (select_str) => {
     // 由于画布并没有像其他的一样支持小程序独有的 rpx 自适应尺寸单位, 所以要获取rpx适配的值
     // 计算公式为 设备视口宽度 / 设计图宽度(375) = 设计图中的一像素适配设备视口宽度的几像素
     const rpx = wx.getSystemInfoSync().windowWidth / 375
+    // 获取canvas实例
+    const node = await new Promise(resolve => {
+        const query = wx.createSelectorQuery()
+        query.select(select_str)
+            .node(({ node }) => resolve(node))
+            .exec()
+    })
+
     // 获取画笔
     const ctx = node.getContext('2d')
 
@@ -44,7 +41,7 @@ export const createDrawRect = (node) => {
      * @param  {number} y 绘制y轴位置(必须)
      * @param  {number} w 绘制图片宽度(必须)
      * @param  {number} h 绘制图片高度(必须)
-     * @returns {Promise} 图片绘制成功时返回true
+     * @returns {Promise} 图片绘制成功时返回true, 需ctx.restore()绘制出实体
      */
     ctx.loadDrawImage = function (path, x, y, w, h) {
         return new Promise(resolve => {
@@ -85,11 +82,11 @@ export const createDrawRect = (node) => {
                 }
             }
         }
-        // 进行绘制
+        // 添加绘制信息
         row.forEach((item, index) => ctx.fillText(item, x, y + index * fontHeight));
     }
 
 
-    // 将封装好的ctx与计算好的rpx返回
-    return {rpx, ctx}
+    // 返回封装
+    return { rpx, ctx, node }
 }
