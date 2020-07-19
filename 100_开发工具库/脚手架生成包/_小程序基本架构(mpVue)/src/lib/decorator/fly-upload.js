952795b5c9e0e25d3908f84931c40177b28110d5
Mr_Mao <wwu710632@gmail.com>
2020-07-19 21:53:07 +0800

    mr_mao 2020年7月19日21:53:00

--- 100_开发工具库/脚手架生成包/_小程序基本架构(mpVue)/src/lib/decorator/fly-upload.js
+++ 100_开发工具库/脚手架生成包/_小程序基本架构(mpVue)/src/lib/decorator/fly-upload.js
@@ -2,7 +2,7 @@
 // 对fly添加upload方法, 模拟fly请求执行环境, 自动调用拦截器和返回结果封装
 export default function (context) {
   // 请求结果处理
-  function handlerResponse(result) {
+  function handlerResponse(result, config) {
     const headers = result.header
     const request = config
     const status = result.statusCode
@@ -16,7 +16,7 @@ export default function (context) {
     return { headers, request, status, statusText, data }
   }
   // 请求错误处理
-  function handlerError(result) {
+  function handlerError(result, config) {
     const message = result.errMsg ? 'request:no' : 'request:ok'
     const request = config
     let data = null
@@ -34,36 +34,39 @@ export default function (context) {
     if (!result.errMsg) {
       return { message, request, response, status }
     }
-    return { message: result.errMsg, request }
+    return { message: result.errMsg, request, status: result.statusCode || 404 }
   }
-  context.upload = function (config = {
-    url: '',
-    name: '',
-    filePath: '',
-    formData: {},
-    baseURL: context.config.baseURL,
-    header: context.config.headers,
-    timeout: context.config.timeout,
-    parseJson: true
-  }) {
+  context.upload = function (config = {}) {
+    config = {
+      url: '',
+      name: '',
+      filePath: '',
+      formData: {},
+      baseURL: context.config.baseURL,
+      header: context.config.headers,
+      timeout: context.config.timeout,
+      parseJson: true,
+      ...config
+    }
     // 保存拦截器
+    const request_handler = context.interceptors.request.handler
     const response_handler = context.interceptors.response.handler
     const response_onerror = context.interceptors.response.onerror
     // 执行uploadFile, 模拟fly环境
     return new Promise((resolve, reject) => {
-      context.interceptors.request.handler(config)
+      request_handler(config)
       wx.uploadFile({
         url: config.baseURL + config.url,
         filePath: config.filePath,
         name: config.name,
         formData: config.formData,
         complete(result) {
-          if (result.status == 200) {
-            const response = handlerResponse(result)
+          if (result.statusCode == 200) {
+            const response = handlerResponse(result, config)
             const res_int = response_handler(response)
             res_int ? resolve(res_int) : resolve(response)
           } else {
-            const error = handlerError(result)
+            const error = handlerError(result, config)
             const err_int = response_onerror(error)
             err_int ? reject(err_int) : reject(error)
             if (!err_int._v) {
