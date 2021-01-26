cfed0c6b50bb6a1dc27c5d99bd3412f04c134cef
TuiMao233 <wwu710632@gmail.com>
2021-01-26 13:41:47 +0800

    📦 NEW: 新增笔记。

--- 00_笔记目录(前端)/page-setup.js
+++ 00_笔记目录(前端)/page-setup.js
@@ -2,7 +2,7 @@
  * @Author: Mr.Mao
  * @LastEditors: Mr.Mao
  * @Date: 2020-12-31 15:03:14
- * @LastEditTime: 2021-01-03 10:55:58
+ * @LastEditTime: 2021-01-22 14:08:27
  * @Description: 
  * @任何一个傻子都能写出让电脑能懂的代码，而只有好的程序员可以写出让人能看懂的代码
  */
@@ -33,8 +33,8 @@ function pageSetup(options) {
 }
 /* 注意:危险操作!! 执行前请备份资料  */
 pageSetup({
-  fastPage: 22,   // 开始页码
-  lastPage: 36,   // 最后页码
+  fastPage: 7,   // 开始页码
+  lastPage: 37,   // 最后页码
   addNumber: 1,  // 每个页码操作多少 默认是0
   pageSplit: '-', // 分隔页码的符号 11-  11.  11_ 等等
   newPageSplit: '-' // 新的分隔符, 不指定时默认对应pageSplit
