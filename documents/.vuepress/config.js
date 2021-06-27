aa03d5fcc8435705388c49af2e1887bf73a1e03b
TuiMao233 <wwu710632@gmail.com>
2021-06-27 18:06:26 +0800

    🎨 修复展示缩放图显示异常。

--- documents/.vuepress/config.js
+++ documents/.vuepress/config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: Mr.Mao
  * @Date: 2021-06-25 22:11:23
- * @LastEditTime: 2021-06-27 15:47:54
+ * @LastEditTime: 2021-06-27 17:55:55
  * @Description: 
  * @LastEditors: Mr.Mao
  * @autograph: 任何一个傻子都能写出让电脑能懂的代码，而只有好的程序员可以写出让人能看懂的代码
